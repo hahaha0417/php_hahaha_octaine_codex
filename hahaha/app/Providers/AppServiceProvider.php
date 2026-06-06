e8e93c5fc26ba538d98efeb6ecdf854b98b4ede3
hahaha <hahaha0417@hotmail.com>
2026-06-06 08:16:34 +0800

    fix

--- hahaha/app/Providers/AppServiceProvider.php
+++ hahaha/app/Providers/AppServiceProvider.php
@@ -3,10 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use L_Lib\Console\Commands\ai\hahaha_cache_ai_context;
-use L_Lib\Console\Commands\ai\hahaha_cache_code_summary;
-use L_Lib\Console\Commands\ai\hahaha_cache_project_structure;
-use L_Lib\Console\Commands\db\hahaha_command_db_table_enum_generate;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -23,13 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        if ($this->app->runningInConsole()) {
-            $this->commands([
-                hahaha_cache_ai_context::class,
-                hahaha_cache_code_summary::class,
-                hahaha_cache_project_structure::class,
-                hahaha_command_db_table_enum_generate::class,
-            ]);
-        }
+        //
     }
 }
--- hahaha/bootstrap/providers.php
+++ hahaha/bootstrap/providers.php
@@ -1,7 +1,9 @@
 <?php
 
 use App\Providers\AppServiceProvider;
+use L_Lib\Providers\hahaha_laravel_lib_service_provider;
 
 return [
     AppServiceProvider::class,
+    hahaha_laravel_lib_service_provider::class,
 ];
